75277d69379184aeb93197c5cd172ba80fb9ec9d
Bala Srinivasan <balatriplee@gmail.com>
2019-06-21 01:48:03 +0530

    Add files via upload

---
+++ src/main/java/sparkStructuredStreaming/KPITable.java
@@ -0,0 +1,32 @@
+package sparkStructuredStreaming;
+
+public class KPITable {
+	
+//	int core_id;
+//	int vendor_id;
+//	int ne_type_id;
+//	String ne_id;
+//	String ne_name;
+//	String object_name;
+//	String ne_date;
+//	int ne_hour;
+//	int ne_minute;
+//	String ne_datetime;
+//	int granularity;
+//	long measurement_id;
+//	String measurement_name;
+//	long counter_id;
+//	String tag;
+//	String sub_ne_type;
+//	String counter_string;
+	
+	String core_id;
+	String vendor_id;
+	String ne_type_id;
+	String ne_id;
+	String object_name;
+	String ne_date;
+	String ne_hour;
+	String ne_minute;
+
+}
